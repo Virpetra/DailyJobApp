de08562f0dd81a8f08e0e27b51fb74161122fba3
Virpetra <msaiddede@gmail.com>
2024-03-14 18:45:43 +0000

    Initial Commit

---
+++ DailyJobApp/DailyJobAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyJobAppApp.swift
+//  DailyJobApp
+//
+//  Created by Mehmet Said Dede on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct DailyJobAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
